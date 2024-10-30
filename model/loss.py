f9e342c4a108edb4888df3de1780e7ff6ff62eb4
Lucas Sobrinho <lucasmsobrinho@gmail.com>
2024-10-30 15:15:12 -0300

    bug fix on categorical cross entropy loss from keras

--- model/loss.py
+++ model/loss.py
@@ -1,4 +1,5 @@
 import torch.nn.functional as F
+import torch
 
 def nll_loss(output, target):
     return F.nll_loss(output, target)
@@ -7,13 +8,18 @@ def cross_entropy_loss(output, target):
     return F.cross_entropy(output, target)
 
 def categorical_cross_entropy_loss(output, target, axis=-1):
+    # output (B, n_classes)
     # categorical_cross_entropy_loss is very different in keras
     # taken from keras implementation with torch backend
     # https://github.com/keras-team/keras/blob/v3.3.3/keras/src/backend/torch/nn.py
     output = output / torch.sum(output, dim=axis, keepdim=True)
-    output = torch.clip(output, 1e-8, 1.0 - 1e-8)
+    eps = torch.finfo(torch.float32).eps
+    output = torch.clip(output, eps, 1.0 - eps)
     log_prob = torch.log(output)
-    return -torch.sum(target * log_prob, dim=axis)
+    # target supposed to be one_hot in keras, so needs to convert target to one_hot
+    target = target.type(dtype=torch.long)
+    target = F.one_hot(target, num_classes=output.shape[1])
+    return torch.mean(-torch.sum(target * log_prob, dim=axis))
 
 
  
