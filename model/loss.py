ab8722ce16b3e08fa178be022337a1468437351f
Lucas Sobrinho <lucasmsobrinho@gmail.com>
2024-10-30 14:36:26 -0300

    added correct categorical_cross_entropy_loss from keras used in Jabbar

--- model/loss.py
+++ model/loss.py
@@ -1,8 +1,19 @@
 import torch.nn.functional as F
 
-
 def nll_loss(output, target):
     return F.nll_loss(output, target)
 
 def cross_entropy_loss(output, target):
     return F.cross_entropy(output, target)
+
+def categorical_cross_entropy_loss(output, target, axis=-1):
+    # categorical_cross_entropy_loss is very different in keras
+    # taken from keras implementation with torch backend
+    # https://github.com/keras-team/keras/blob/v3.3.3/keras/src/backend/torch/nn.py
+    output = output / torch.sum(output, dim=axis, keepdim=True)
+    output = torch.clip(output, 1e-8, 1.0 - 1e-8)
+    log_prob = torch.log(output)
+    return -torch.sum(target * log_prob, dim=axis)
+
+
+ 
