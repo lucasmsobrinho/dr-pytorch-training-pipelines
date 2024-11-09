e1419d665a511f6020f7cd987d8a5bf86286b5be
Lucas Sobrinho <lucasmsobrinho@gmail.com>
2024-11-09 08:45:31 -0300

    refactor mlflow populate script

--- utils/mlops.py
+++ utils/mlops.py
@@ -2,56 +2,70 @@ import mlflow
 from tensorboard.backend.event_processing import event_accumulator
 import json
 import glob
+import os
 
-base_path ="/mnt/c/Users/tex/Downloads/saved"
-events_path = f"{base_path}/saved/log/*/*/events*"
-eps = glob.glob(events_path)
-print(len(eps))
+def get_experiments(base_path, exp_pattern):
+    exps = []
+    test_paths = glob.glob(f"{base_path}/log/{exp_pattern}/*/test.log")
+    for test_log_path in test_paths:
+        experiment, run = test_log_path.split("/")[-3:-1]
+        event_path = glob.glob(f"{base_path}/log/{experiment}/{run}/events*")
+        train_log_path = f"{base_path}/log/{experiment}/{run}/info.log"
+        config_path = f"{base_path}/models/{experiment}/{run}/config.json"
+        #model_best = f"{base_path}/models/{experiment}/{run}/model_best.pth"
+        if len(event_path) == 0:
+            continue
+        event_path = event_path[0]
+        paths = [event_path, train_log_path, test_log_path, config_path] #, model_best]
+        if all(os.path.exists(path) for path in paths):
+            exps.append(paths)
+    return exps
 
-for ep in eps:
-    print(f"run {ep}")
-    experiment, run = ep.split('/')[-3:-1]
-    config_path = f"{base_path}/saved/models/{experiment}/{run}/config.json"
+if __name__ == "__main__":
+    base_path = "/mnt/c/Users/tex/Downloads/saved"
+    base_path = "/root/saved"
+    exp_pattern = "01*"
+    exps = get_experiments(base_path, exp_pattern)
 
-    try:
-        with open(config_path) as f:
-            config = json.load(f)
+    if len(exps) == 0:
+        print("No experiments found.")
+    else:
+        print(f"{len(exps)} experiments found.")
 
-        with mlflow.start_run(run_name=run):
-            # loading config file for run parameters
-            for main_key, value in config.items():
-                if type(value) == dict:
-                    for sub_key, sub_value in value.items():
-                        if type(sub_value) == dict:
-                            for sub_sub_key, sub_sub_value in sub_value.items():
-                                # Use "main_key.sub_key" format for hierarchical logging
-                                mlflow.log_param(f"{main_key}.{sub_key}.{sub_sub_key}", sub_sub_value)
-                        else:
-                            mlflow.log_param(f"{main_key}.{sub_key}", sub_value)
-                else:
-                    mlflow.log_param(main_key, value)
+    for event_path, train_log_path, test_log_path, config_path in exps:
+        experiment, run = config_path.split('/')[-3:-1]
 
-            # load metrics from tensorboard
-            ea = event_accumulator.EventAccumulator(ep)
-            ea.Reload()
-            for tag in ea.Tags()['scalars']:
-                print("\ttag:", tag)
-                events = ea.Scalars(tag)
-                for e in events:
-                    # Log each metric from TensorBoard to MLflowea
-                    mlflow.log_metric(tag, e.value, step=e.step)
+        try:
+            # load config.json
+            with open(config_path) as f:
+                config = json.load(f)
 
-            # load info.log
-            info_path = f"{base_path}/saved/log/{experiment}/{run}/info.log"
-            mlflow.log_artifact(info_path, "info.log")
-            try:
-                test_path = f"{base_path}/saved/log/{experiment}/{run}/test.log"
-                mlflow.log_artifact(test_path, "test.log")
-            except Exception as e:
-                print(e)
+            with mlflow.start_run(run_name=f"{experiment}/{run}"):
+                # load config file
+                for main_key, value in config.items():
+                    if type(value) == dict:
+                        for sub_key, sub_value in value.items():
+                            if type(sub_value) == dict:
+                                for sub_sub_key, sub_sub_value in sub_value.items():
+                                    # Use "main_key.sub_key" format for hierarchical logging
+                                    mlflow.log_param(f"{main_key}.{sub_key}.{sub_sub_key}", sub_sub_value)
+                            else:
+                                mlflow.log_param(f"{main_key}.{sub_key}", sub_value)
+                    else:
+                        mlflow.log_param(main_key, value)
 
+                # load metrics from tensorboard
+                ea = event_accumulator.EventAccumulator(event_path)
+                ea.Reload()
+                for tag in ea.Tags()['scalars']:
+                    print("\ttag:", tag)
+                    events = ea.Scalars(tag)
+                    for e in events:
+                        # Log each metric from TensorBoard to MLflowea
+                        mlflow.log_metric(tag, e.value, step=e.step)
 
-    except Exception as e:
-        print(e)
+                mlflow.log_artifact(train_log_path, "info.log")
+                mlflow.log_artifact(test_log_path, "test.log")
 
-            # TODO: load train, valid, test final metrics
+        except Exception as e:
+            print(e)
